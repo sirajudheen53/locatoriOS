3bb78f0ae63b2a3af1a6be18bff1bc3e5f2e6fdc
sirajudheen <sirajudheen@qburst.com>
2015-03-11 11:36:31 +0530

    Initial Commit

---
+++ Locator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Locator
+//
+//  Created by QBUser on 11/03/15.
+//  Copyright (c) 2015 QBUser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
